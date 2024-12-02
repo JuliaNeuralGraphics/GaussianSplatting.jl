da3e2c62286d51702ff32f2e5e5ef0534f8eab18
Anton Smirnov <tonysmn97@gmail.com>
2024-12-02 02:13:38 +0200

    Add docs

---
+++ docs/make.jl
@@ -0,0 +1,32 @@
+using Documenter
+using GaussianSplatting
+
+function main()
+    ci = get(ENV, "CI", "") == "true"
+
+    @info "Generating Documenter site"
+    makedocs(;
+        modules=[GaussianSplatting],
+        sitename="GaussianSplatting.jl",
+        format=Documenter.HTML(;
+            prettyurls=ci,
+            assets=["assets/favicon.ico"],
+            analytics="UA-154489943-2",
+        ),
+        warnonly=[:missing_docs],
+        pages=[
+            "Home" => "index.md",
+            "Examples" => "examples.md",
+            "API" => "api.md",
+        ],
+    )
+    if ci
+        @info "Deploying to GitHub"
+        deploydocs(;
+            repo="github.com/JuliaNeuralGraphics/GaussianSplatting.jl.git",
+            push_preview=true,
+        )
+    end
+end
+
+isinteractive() || main()
