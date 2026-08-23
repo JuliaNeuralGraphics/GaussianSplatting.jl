cae746aad584a858e03b969cc7bd6472459876c9
Anton Smirnov <tonysmn97@gmail.com>
2026-08-23 23:30:16 +0300

    Precompile workloads for CUDA & Metal

--- ext/GaussianSplattingCUDAExt.jl
+++ ext/GaussianSplattingCUDAExt.jl
@@ -2,13 +2,13 @@ module GaussianSplattingCUDAExt
 
 using CUDA
 using GaussianSplatting
+using PrecompileTools: @compile_workload
 
 GaussianSplatting.base_array_type(::CUDABackend) = CuArray
 
 function GaussianSplatting.allocate_pinned(::CUDABackend, ::Type{T}, shape) where T
     x = Array{T}(undef, shape)
-    buf = CUDA.register(CUDA.HostMemory, pointer(x), sizeof(x),
-        CUDA.MEMHOSTREGISTER_DEVICEMAP)
+    buf = CUDA.register(CUDA.HostMemory, pointer(x), sizeof(x), CUDA.MEMHOSTREGISTER_DEVICEMAP)
     xptr = convert(CuPtr{Float32}, buf)
     xd = unsafe_wrap(CuArray, xptr, size(x))
     return x, xd
@@ -17,6 +17,18 @@ end
 function GaussianSplatting.unpin_memory(x::CuArray)
     CUDA.unregister(x.data.rc.obj.mem)
     return
+end
+
+@compile_workload begin
+    if Base.JLOptions().check_bounds != 1 && Base.JLOptions().code_coverage == 0 &&
+        CUDA.functional() && !isempty(CUDA.devices())
+        try
+            GaussianSplatting.precompile_workload(CUDABackend())
+        catch e
+            @debug "CUDA precompile workload failed." exception=(e, catch_backtrace())
+        end
+        GC.gc(true)
+    end
 end
 
 end
--- ext/GaussianSplattingMetalExt.jl
+++ ext/GaussianSplattingMetalExt.jl
@@ -2,6 +2,7 @@ module GaussianSplattingMetalExt
 
 using Metal
 using GaussianSplatting
+using PrecompileTools: @compile_workload
 
 GaussianSplatting.base_array_type(::MetalBackend) = MtlArray
 
@@ -15,5 +16,17 @@ end
 
 # Unregistered automatically in the array dtor.
 GaussianSplatting.unpin_memory(::MtlArray) = return
+
+@compile_workload begin
+    if Base.JLOptions().check_bounds != 1 && Base.JLOptions().code_coverage == 0 &&
+        Metal.functional() && !isempty(Metal.devices())
+        try
+            GaussianSplatting.precompile_workload(MetalBackend())
+        catch e
+            @debug "Metal precompile workload failed." exception=(e, catch_backtrace())
+        end
+        GC.gc(true)
+    end
+end
 
 end
